c1f56bbc910741be3a8bdf68220685cce7cc3505
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-20 00:27:02 +0200

    Update init_raycasting.c

--- src/init_raycasting.c
+++ src/init_raycasting.c
@@ -128,7 +128,7 @@ void	setup_ray_dir(t_raycaster *rc, t_player *player, int x)
 {
 	rc->x = x;
 	rc->camera_x = 2 * x / (double)WIDTH - 1;
-	rc->raydirx = player->direction->x + player->plane->x * rc->camera_x;
-	rc->raydiry = player->direction->y + player->plane->y * rc->camera_x;
+	rc->raydirx = player->direction.x + player->plane.x * rc->camera_x;
+	rc->raydiry = player->direction.y + player->plane.y * rc->camera_x;
 	fill_variables(rc[x], player->position->x, player->position->y);
 }
