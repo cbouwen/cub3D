e5fca3abd2b0fb30e145c266b818d8b15b7c7a04
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-20 20:13:01 +0200

    Update execute.c

--- src/execute.c
+++ src/execute.c
@@ -113,9 +113,9 @@ void	calculate_dda(t_raycaster *rc, t_map **map, t_data *img)
 			hit = 1;
 		print_debug(rc, "calculate_dda_after_if_3");
 	}
-	print_debug(rc, "calculate_dda_after_while");
-	calculate_lineheight(rc, rc->side);
-	print_debug(rc, "calculate_dda_after_lineheight");
+	//print_debug(rc, "calculate_dda_after_while");
+	//calculate_lineheight(rc, rc->side);
+	//print_debug(rc, "calculate_dda_after_lineheight");
 	load_texture(rc, rc->side, img);
 	print_debug(rc, "calculate_dda_after_texture");
 }
