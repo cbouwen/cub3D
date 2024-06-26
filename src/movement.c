62788a79e40084703cc53dd88a4de478e480b4ac
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-26 22:56:02 +0200

    some old references

--- src/movement.c
+++ src/movement.c
@@ -2,15 +2,13 @@
 
 void	update_player(new_t_data *data)
 {
-	t_mlx_data	win_data;
-
 	data->mlx = mlx_init();
 	init_window(data);
 	/*draw_screen(rc, &win_data.img, mapinfo);
 	mlx_put_image_to_window(win_data.mlx, win_data.mlx_win, win_data.img.img, 0, 0);*/
 //	mlx_loop_hook(win_data.mlx, 
 //	mlx_key_hook(win_data.mlx_win, handle_input, &win_data);
-	mlx_loop(win_data.mlx);
+	mlx_loop(data->mlx);
 
 
 
