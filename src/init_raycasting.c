db675bc66c693b346cb7386179514d45442922fe
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-26 21:51:37 +0200

    fucked up the variables a bit

--- src/init_raycasting.c
+++ src/init_raycasting.c
@@ -1,13 +1,13 @@
 #include "../inc/cub3d.h"
 
-void	setup_ray_dir(new_t_data *data, int x)
+void	setup_ray_dir(new_t_player *rc, new_t_player *player, int x)
 {
-	data->rc.x = x;
-	data->rc.camera_x = 2 * x / (double)WIDTH - 1;
-	if (data->rc.camera_x == 0)
-		data->rc.camera_x = 2 * (x - 1) / (double)WIDTH - 1;//hardcoded af. run tests if this doesn't give issues elsewhere. Check with other students how they fixed it
-	data->rc.raydirx = data->player.direction.x + data->player.plane.x * data->rc.camera_x;
-	data->rc.raydiry = data->player.direction.y + data->player.plane.y * data->rc.camera_x;
+	rc->x = x;
+	rc->camera_x = 2 * x / (double)WIDTH - 1;
+	if (rc->camera_x == 0)
+		rc->camera_x = 2 * (x - 1) / (double)WIDTH - 1;//hardcoded af. run tests if this doesn't give issues elsewhere. Check with other students how they fixed it
+	rc->raydirx = player->direction.x + player->plane.x * rc->camera_x;
+	rc->raydiry = player->direction.y + player->plane.y * rc->camera_x;
 	//init_tile_traversal(rc, player->position.x, player->position.y);
 }
 
@@ -18,7 +18,7 @@ void	init_raycasting(new_t_data *data)
 	x = 0;
 	while (x < WIDTH)
 	{
-		setup_ray_dir(data[x], x);
+		setup_ray_dir(data->rc[x], data->player, x);
 		x++;
 	}
 }
