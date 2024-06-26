cec3b4063f7afb7212c2705e479e8dc418402ca9
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-26 21:53:15 +0200

    Update cub3d.h

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -82,7 +82,7 @@ void		execute_map(new_t_data *data);
 
 /*init_raycasting.c*/
 void		init_raycasting(new_t_data *data);
-void		setup_ray_dir(new_t_player *rc, new_t_player *player, int x);
+void		setup_ray_dir(new_t_raycaster *rc, new_t_player *player, int x);
 
 //parser
 /*int			parse_cub(char *argv, t_mapinfo *mapinfo, t_map ***map);
