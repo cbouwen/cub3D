990c056e803ac2854c51d0e8a4ac7fac9cd43f3d
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-26 19:37:19 +0200

    Update valid_map.c

--- src/parser/valid_map.c
+++ src/parser/valid_map.c
@@ -5,7 +5,7 @@ int	check_open(new_t_data *data, int y, int x)
 	if (data->mapinfo.map[y][x].c == '0' || data->mapinfo.map[y][x].c == 'N' || data->mapinfo.map[y][x].c == 'S'
 		|| data->mapinfo.map[y][x].c == 'E' || data->mapinfo.map[y][x].c == 'W')
 	{
-		if (y == 0 || x == 0 || y == data->mapinfo.rows - 1 || x == data->mapinfo.colomns - 1)
+		if (y == 0 || x == 0 || y == data->mapinfo.rows - 1 || x == data->mapinfo.columns - 1)
 			return (0);
 		if (y > 0 && data->mapinfo.map[y - 1][x].c == 32)
 			return (0);
@@ -13,7 +13,7 @@ int	check_open(new_t_data *data, int y, int x)
 			return (0);
 		if (y < data->mapinfo.rows && data->mapinfo.map[y + 1][x].c == 32)
 			return (0);
-		if (x > data->mapinfo.colomns && data->mapinfo.map[y][x + 1].c == 32)
+		if (x > data->mapinfo.columns && data->mapinfo.map[y][x + 1].c == 32)
 			return (0);
 	}
 	return (1);
@@ -34,12 +34,12 @@ int	valid_map(new_t_data *data)
 
 	player = 0;
 	y = 0;
-	if (data->mapinfo.rows < 3 || data->mapinfo.colomns < 3)
+	if (data->mapinfo.rows < 3 || data->mapinfo.columns < 3)
 		return (0);
 	while (y < data->mapinfo.rows)
 	{
 		x = -1;
-		while (++x < data->mapinfo.colomns)
+		while (++x < data->mapinfo.columns)
 		{
 			if (!check_open(data, y, x))
 				return (0);
