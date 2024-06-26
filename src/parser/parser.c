8210c62bd5535888b9de65fd601faa7d199db14d
LegendreMatisse <legendre.matisse@gmail.com>
2024-06-26 18:14:16 +0200

    re-added map_errors function

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -9,6 +9,18 @@ int	color_range(int	x)
 	return (1);
 }
 
+void	map_errors(t_mapchecker *elements, new_t_data *data)
+{
+	if (elements->all_eles == false)
+		ft_error("Not all elements are defined before map initialization.\n");
+	if (elements->duplicate == true)
+		ft_error("Duplicate map arguments.\n");
+	if (!color_range(data->mapinfo.c))
+		ft_error("Wrong color ranges for ceiling\n");
+	if (!color_range(data->mapinfo.f))
+		ft_error("Wrong color ranges for floor\n");
+}
+
 void	parse_mapinfo(char *str, new_t_data *data)
 {
 	int	i;
@@ -42,6 +54,18 @@ void	parse_input(int fd, new_t_data *data, new_t_mapchecker *elements)
 	}
 }
 
+void print_mapchecker(new_t_mapchecker elements)
+{
+	printf("no: %d\n", elements.no);
+	printf("so: %d\n", elements.so);
+	printf("we: %d\n", elements.we);
+	printf("ea: %d\n", elements.ea);
+	printf("c: %d\n", elements.c);
+	printf("f: %d\n", elements.f);
+	printf("all_eles: %d\n", elements.all_eles);
+	printf("duplicate: %d\n", elements.duplicate);
+}
+
 int	parse_cub(char *argv, new_t_data *data)
 {
 	new_t_mapchecker	elements;
@@ -53,8 +77,9 @@ int	parse_cub(char *argv, new_t_data *data)
 		ft_error("Error opening map.. Weird. Try again!\n");
 	parse_input(fd, data, &elements);
 	close(fd);
-	/*map_errors(elements, *mapinfo);
-	fd = open(argv, O_RDONLY);
+	map_errors(&elements, data);
+	print_mapchecker(elements);
+	/*fd = open(argv, O_RDONLY);
 	if (fd == -1)
 		ft_error("Error opening map.. Weird. Try again!\n");
 	parse_map(fd, *mapinfo, map);
