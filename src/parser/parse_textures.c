caf42ef00175691ddd8fc8c4f1ae8cd29ef5e631
LegendreMatisse <legendre.matisse@gmail.com>
2024-07-17 16:37:03 +0200

    Update parse_textures.c

--- src/parser/parse_textures.c
+++ src/parser/parse_textures.c
@@ -23,6 +23,7 @@ void	parse_texture(t_data *data)
 		data->img.addr[y * width + x] = temp[y * (line_length / 4) + x]
 	*/
 	printf("1\n");
+	printf("NO: %s\n", data->mapinfo.no);
 	data->text[NORTH].img = mlx_xpm_file_to_image(data->mlx, data->mapinfo.no, &data->text[NORTH].width, &data->text[NORTH].height);
 	printf("2\n");
 	if (!data->text[NORTH].img)
