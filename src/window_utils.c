993c777c7f0a4a98454a64dbca364fe460aaa7ba
LegendreMatisse <legendre.matisse@gmail.com>
2024-07-17 21:06:56 +0200

    Update window_utils.c

--- src/window_utils.c
+++ src/window_utils.c
@@ -114,6 +114,7 @@ static int	determine_pixel_hit(t_raycaster *rc, t_data *data, int side, double w
 
 	x = (int)(wallpos * data->text[side].width) % data->text[side].width;
 	step = 1.0 * data->text[side].height / rc->lineheight;
+	printf("lineheight: %i\n", rc->lineheight);
 	printf("step: %f\n", step);
 	data->pos = (rc->drawstart - HEIGHT / 2 + rc->lineheight / 2) * step;
 	printf("pos: %f\n", data->pos);
