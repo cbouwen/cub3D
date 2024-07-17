c822a4fb869cf0f2761a256e4dab8603678d6835
LegendreMatisse <legendre.matisse@gmail.com>
2024-07-17 16:43:16 +0200

    test to see if path is correct

--- src/parser/parse_textures.c
+++ src/parser/parse_textures.c
@@ -24,7 +24,7 @@ void	parse_texture(t_data *data)
 	*/
 	printf("1\n");
 	printf("NO: %s\n", data->mapinfo.no);
-	data->text[NORTH].img = mlx_xpm_file_to_image(data->mlx, data->mapinfo.no, &data->text[NORTH].width, &data->text[NORTH].height);
+	data->text[NORTH].img = mlx_xpm_file_to_image(data->mlx, data->mapinfo.no + 3, &data->text[NORTH].width, &data->text[NORTH].height);
 	printf("2\n");
 	if (!data->text[NORTH].img)
 		ft_error("Error\nTexture not found");
