3da9c69973c8dc97995cc978e3c0406b663bf729
Cedric Bouwen <cbouwen@a2-r5-p5.s19.be>
2024-07-23 16:54:49 +0200

    Unconditonal jump fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlegendr <mlegendr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:41:59 by cbouwen           #+#    #+#             */
-/*   Updated: 2024/07/23 16:32:20 by mlegendr         ###   ########.fr       */
+/*   Updated: 2024/07/23 16:53:57 by cbouwen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	t_data	data;
 
 	if (argc != 2)
-		ft_error("Wrong number of arguments. Try again!\n", &data);
+		ft_error("Wrong number of arguments. Try again!\n", NULL);
 	else if (!(map_name(argv[1])))
-		ft_error("Map doesn't end with .cub\n", &data);
+		ft_error("Map doesn't end with .cub\n", NULL);
 	else
 	{
 		init_data_default(&data);
