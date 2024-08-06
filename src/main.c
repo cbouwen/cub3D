c271aee22d1ac50e3f55c3f88b80c65022e97f56
Cedric Bouwen <cbouwen@a1-r2-p7.s19.be>
2024-08-06 16:14:55 +0200

    Fixed the freeing at the start of the program....

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlegendr <mlegendr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:41:59 by cbouwen           #+#    #+#             */
-/*   Updated: 2024/07/31 16:46:58 by mlegendr         ###   ########.fr       */
+/*   Updated: 2024/08/06 16:09:49 by cbouwen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	t_data	data;
 
 	if (argc != 2)
-		ft_error("Wrong number of arguments. Try again!\n", &data);
+		ft_error("Wrong number of arguments. Try again!\n", NULL);
 	else if (!(map_name(argv[1])))
-		ft_error("Map doesn't end with .cub\n", &data);
+		ft_error("Map doesn't end with .cub\n", NULL);
 	else
 	{
 		init_data_default(&data);
